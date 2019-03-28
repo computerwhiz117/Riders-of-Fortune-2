311352263bd3c0118885ba2c58d56fd8b2aa5e00
computerwhiz117 <computerwhiz117@gmail.com>
2019-03-27 22:42:24 -0400

    First commit

---
+++ Riders of Fortune 2/saveGame.cpp
@@ -0,0 +1,17 @@
+// Saves the player's state in the Riders of Fortune 2.0 game
+
+#include "pch.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int _tmain(int argc,_TCHAR* argv[])
+{
+	string name;
+	ifstream takeData;
+	takeData.open("data.dat");
+
+	takeData 
+	return 0;
+}
